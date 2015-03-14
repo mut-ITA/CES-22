46ca0b754bc8d073550d2141cf7abe7c44eccc58
ghust1995 <gustavo.ceci95@gmail.com>
2015-03-14 02:26:30 -0300

    Added calculator project

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Calculator implements ICalculator
+{
+	private double value;
+	private double keep;
+	private String operation;
+	private String errorMessage = "";
+	
+	private List<String> digits = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "0");
+	
+	private OperationManager operationManager = new OperationManager();
+		
+	public void Input(String input)
+	{
+		errorMessage = "";
+		
+		if(digits.contains(input))
+		{
+			Digit(Double.parseDouble(input));
+		}
+		else if(operationManager.operations.contains(input))
+		{			
+			if(operation != null) value = operationManager.Operate(keep, value, operation);
+			keep = value;
+			value = 0;
+			operation = input;			
+		}
+		else if (input.equals("="))
+		{
+			value = operationManager.Operate(keep, value, operation);
+			keep = 0;
+		}
+		else
+		{
+			errorMessage = "Invalid input";
+		}
+	}
+	
+	public String Output()
+	{
+		if(!errorMessage.equals("")) return errorMessage;
+		
+		return String.valueOf(value);
+	}
+	
+	private void Digit(double digit)
+	{
+		value = 10 * value + digit;
+	}
+}
